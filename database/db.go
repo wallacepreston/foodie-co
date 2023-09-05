ce5f20344550264d99f393b1f335fe24b0a01812
Preston Wallace <wallace.preston@gmail.com>
2023-09-05 10:27:50 -0700

    formatting

--- database/db.go
+++ database/db.go
@@ -3,23 +3,23 @@
 package database
 
 import (
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func InitDatabase(dsn string) (*gorm.DB, error) {
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        return nil, err
-    }
-    DB = db
-    return db, nil
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	DB = db
+	return db, nil
 }
 
 type Recipe struct {
-    gorm.Model
-    Name        string
-    Instructions string
+	gorm.Model
+	Name         string
+	Instructions string
 }
--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
-	
+
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
-	"log"
+	"fmt"
 	"foodie-co/database"
+	"log"
+	"net/http"
+
 	_ "github.com/lib/pq"
 )
 
 type Recipe struct {
-	ID          int    `json:"recipe_id"`
-	Name        string `json:"name"`
+	ID           int    `json:"recipe_id"`
+	Name         string `json:"name"`
 	Instructions string `json:"instructions"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
 }
 
 func main() {
@@ -22,28 +23,28 @@ func main() {
 	dsn := "user=postgres dbname=foodie sslmode=disable"
 	db, err := database.InitDatabase(dsn)
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 	// log out db
 	fmt.Println(db)
 
 	// Server setup
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World!")
 	})
 	http.HandleFunc("/recipes", func(w http.ResponseWriter, r *http.Request) {
-    var recipes []database.Recipe // Use the Recipe struct from the database package
+		var recipes []database.Recipe // Use the Recipe struct from the database package
 
-    // Query the "recipes" table using GORM
-    database.DB.Find(&recipes)
+		// Query the "recipes" table using GORM
+		database.DB.Find(&recipes)
 
-    // Convert the results to JSON and send the response
-    w.Header().Set("Content-Type", "application/json")
-    err := json.NewEncoder(w).Encode(recipes)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		// Convert the results to JSON and send the response
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(recipes)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 
 	// Start the server
